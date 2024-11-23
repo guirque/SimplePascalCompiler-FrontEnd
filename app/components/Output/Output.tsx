de67e93d9076ba0b8b77e19a98e768c1bc453b40
Guilherme <guirque@gmail.com>
2024-11-23 16:18:50 -0300

    feat: adicionando code highlight para analise lexica

--- app/components/Output/Output.tsx
+++ app/components/Output/Output.tsx
@@ -1,8 +1,9 @@
+import compilationResponse from "@/app/Interfaces/compilationResponse";
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { JsxElement } from "typescript";
 
 import style from './Output.module.css'
 import PhaseButton from "./PhaseButton/PhaseButton";
-import compilationResponse from "@/app/Interfaces/compilationResponse";
 
 const Output = (params:any) =>
 {
@@ -10,12 +11,24 @@ const Output = (params:any) =>
     const [currentPhase, setCurrentPhase] = useState('Lexical');
 
     let data: compilationResponse = params.output;
-    let outputData = `Click 'Run' to view an output.`;
+    let outputData: string | React.JSX.Element[] = `Click 'Run' to view an output.`;
+    let elementOutput: Element[];
 
     if(data.lexical) switch(currentPhase)
     {
         case 'Lexical':
-            outputData = JSON.stringify(data.lexical).replace(/},/g, '},\n');
+            const size = data.lexical.length;
+            outputData = data.lexical.map(
+                (aToken: {lexema:string, classification:string, line:number}, i)=>
+                {
+                    return <code key={`${aToken.lexema}${i}`} className={style.code}>
+                        {i != 0 ? '{' : '[{'}
+                        <div className={style.propertyHighlight}>{` lexema: `}</div>{aToken.lexema}{','}
+                        <div className={style.propertyHighlight}>{` classification: `}</div>{aToken.classification}{','}
+                        <div className={style.propertyHighlight}>{` line: `}</div>{aToken.line}
+                        {i != size-1 ? '},' : '}]'}
+                        </code>
+                });
             break;
         case 'Syntactic':
             outputData = JSON.stringify(data.syntatic);
