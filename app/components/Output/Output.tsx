4e10064da5ac5d697efad236057109f3324ea1f3
Guilherme <guirque@gmail.com>
2024-11-27 22:01:01 -0300

    feat: adicionando renderizacao de analise sintatica

--- app/components/Output/Output.tsx
+++ app/components/Output/Output.tsx
@@ -5,6 +5,30 @@ import { JsxElement } from "typescript";
 import style from './Output.module.css'
 import PhaseButton from "./PhaseButton/PhaseButton";
 
+type token = {lexema:string, classification:string, line:number};
+type treeValue = {value: string, type: "TERMINAL" | "NON-TERMINAL"};
+
+class tree 
+{
+    public children: tree[] = [];
+    constructor(public value: treeValue){}
+
+    addChild(node: tree)
+    {
+        this.children.push(node);
+    }
+}
+
+function renderTree(node: tree): React.JSX.Element
+{
+    if(node.children.length >= 1)
+    {
+        let childrenElements = node.children.map((child:tree)=> renderTree(child));
+        return <div className={`${style.node} ${node.value.type == 'NON-TERMINAL' ? style.nonterminal : style.terminal}`} key={node.value.value}><div>{node.value.value}</div> <div className={style.nodechildren}>{childrenElements}</div> </div>;
+    }
+    else return <div className={`${style.node} ${node.value.type == 'NON-TERMINAL' ? style.nonterminal : style.terminal}`} key={node.value.value}>{node.value.value}</div>;
+}
+
 const Output = (params:any) =>
 {
 
@@ -19,7 +43,7 @@ const Output = (params:any) =>
         case 'Lexical':
             const size = data.lexical.length;
             outputData = data.lexical.map(
-                (aToken: {lexema:string, classification:string, line:number}, i)=>
+                (aToken: token, i)=>
                 {
                     return <code key={`${aToken.lexema}${i}`} className={style.code}>
                         {i != 0 ? '{' : '[{'}
@@ -31,7 +55,8 @@ const Output = (params:any) =>
                 });
             break;
         case 'Syntactic':
-            outputData = JSON.stringify(data.syntatic);
+            outputData = [renderTree(data.syntatic)];
+            //outputData = JSON.stringify(data.syntatic);
             break;
         case 'Semantic':
             outputData = JSON.stringify(data.semantic);
